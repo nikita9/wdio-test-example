028e1b3d975f9980a2c26196bd0a3bb413cde734
nikita gerasimenko <nikitag@leadsmarket.com>
2020-02-07 09:23:44 -0800

    Initial setup

--- wdio.conf.js
+++ wdio.conf.js
@@ -48,6 +48,7 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
+    //
     capabilities: [{
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
